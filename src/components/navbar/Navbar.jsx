e130e4b2156b4fc4b8c35698bba839b6e4145bd6
DrMetr <metronomedragon@gmail.com>
2025-09-29 20:44:32 +0300

    Navbar added in order to carry out a test

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,14 @@
 export default function Navbar() {
-  return;
+  const navbarStyle = {
+    display: 'flex', 
+    flexDirection: 'row',
+    gap: '10px'
+  }
+  return(<div style={navbarStyle}>
+    <a>Главная</a>
+    <a>О нас</a>
+    <a>Услуги</a>
+    <a>Кейсы</a>
+    <a>Контакты</a>
+    </div>);
 }
