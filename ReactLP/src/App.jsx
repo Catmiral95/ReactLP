b3b11fa50468a6c1d64c301a1b360c67c03b8b81
Catmiral95 <catmiral@yandex.ru>
2026-04-04 21:18:59 +0300

    new updates
    new updates

--- ReactLP/src/App.jsx
+++ ReactLP/src/App.jsx
@@ -1,49 +1,3 @@
-/*import "./App.css";
-import Header from "./components/header/HeaderNavbar.jsx";
-import About from "./components/about/About.jsx";
-import LegalFees from "./components/legalFees/LegalFees.jsx";
-import Cases from "./components/cases/cases.jsx";
-import Contacts from "./components/contacts/Contacts.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import Consent from "./components/consent/Consent.jsx";
-import Hero from "./components/hero/Hero.jsx";
-import ScrollUp from "./components/scroll up/ScrollUpBtn.jsx";
-import { useState, useEffect } from "react";
-
-function App() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-
-
-  return (
-    <>
-      <Header windowWidth={windowWidth} />
-      {windowWidth > 1200 && <ScrollUp />}
-      <main>
-        <Hero windowWidth={windowWidth} />
-        <About windowWidth={windowWidth} />
-        <Consent />
-        <LegalFees />
-        <Cases windowWidth={windowWidth} />
-        <Contacts windowWidth={windowWidth} />
-      </main>
-      <Footer />
-    </>
-  );
-}
-
-export default App;
-*/
 
 import "./App.css";
 import Header from "./components/header/HeaderNavbar.jsx";
@@ -58,7 +12,7 @@ import ScrollUp from "./components/scroll up/ScrollUpBtn.jsx";
 import PrivacyPolicy from "./components/additional pages/PrivacyPolicy.jsx";
 import ConsentText from "./components/additional pages/ConsentText.jsx";
 import { useState, useEffect } from "react";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
